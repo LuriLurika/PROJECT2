a0755e6360ccfe65c33f864fad566c1470f5d5d5
Victor Sanchez <victor1305@icloud.com>
2020-07-06 14:54:24 +0200

    Routas Catlogo y Profile  OK, roles en Catalogo OK

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -3,10 +3,13 @@ const router = express.Router()
 const Car = require('../models/car.model')
 const User = require('../models/user.model')
 
+const fuelArray = ['Diésel', 'Gasolina', 'Eléctrico', 'Híbrido', 'Híbrido Enchufable', 'Gas Licuado', 'Gas Natural', 'Otros']
+const typeCarArray = ['Berlina', 'Familiar', 'Coupe', 'Monovolumen', '4x4 SUV', 'Cabrio', 'Pick Up']
+
 
 //INDEX PERFIL
 
-router.get('/profile',  (req, res) => {
+router.get('/',  (req, res) => {
 
     User
         .findOne(req.params.id)
@@ -16,44 +19,83 @@ router.get('/profile',  (req, res) => {
 
 //CREAR ANUNCIO
 
-router.get('/new', (req, res) => res.send('CREA TU ANUNCIO'))
+router.get('/new', (req, res) => {
+    
+    Car
+        .find()
+        .then(allCars => res.render("catalog/new", {allCars, fuelArray, typeCarArray}))
+        .catch(err => console.log('BBDD error', err))
+
+})
+
 router.post('/new', (req, res) => {
+
     const { brand, model, year, color, fuel, type, price, photo, description } = req.body
     
     Car
         .create({ brand, model, year, color, fuel, type, price, photo, description })
-        .then(response => {
-            res.redirect('/new')
+        .then( newCar => {
+            console.log("Nuevo coche añadido", newCar)
+            res.redirect("/profile/show-car")
         })
         .catch(err => console.log('Error en la BBDD', err))
 })
 
 //VER TUS ANUNCIOS
 
-router.get('/ver', (req, res) => res.send('ESTOS SON TUS COCHES EN VENTA'))
+router.get('/show', (req, res) => res.render('profile/show-car'))
 
 //MODIFICAR ANUNCIOS
 
-router.get('/editCar', (req, res) => res.send('MODIFICA TU ANUNCIO'))
-router.get('/editCar/:id', (req, res) => {
+router.get('/:id/edit-car', (req, res) => {
+    
     Car
         .findById(req.params.id)
-        .then(theCar => res.render('/editCar', { theCar }))
+        .then(theCar => res.render('catalog/edit', {theCar, fuelArray, typeCarArray}))
+        .catch(err => ('Error en la BBDD', err))
+
+})
+
+router.post('/:id', (req, res) => {
+
+    const { brand, model, year, color, fuel, type, price, photo, description } = req.body
+
+    Car
+        .findById(req.params.id, { brand, model, year, color, fuel, type, price, photo, description }, {new: true})
+        .then(() => res.redirect('profile/show-car'))
         .catch(err => ('Error en la BBDD', err))  
 })
 
 //ELIMINAR ANUNCIOS
 
-router.get('/delete', (req, res) => res.send('ELIMINAR ANUNCIO'))
+router.post('/:id/delete', (req, res) => {
+
+    Car
+        .findByIdAndRemove(req.params.id)
+        .then(() => res.redirect('/profile/show-car'))
+        .catch(err => ('Error en la BBDD', err))  
+})
 
 //MODIFICAR PERFIL
 
-router.get('/editProfile', (req, res) => res.send('MODIFICA TU PERFIL'))
-router.post('/editProfile', (req, res) => {
+router.get('/:id/edit-profile', (req, res) => {
+    
+    User    
+        .findById(req.params.id)
+        .then(theUser => res.render("profile/edit-profile", theUser))
+        .catch(err => console.log('BBDD error', err))
+})
+
+router.post('/:id', (req, res) => {
+
+    const { username, password } = req.body
+
     User
-        .then(theUser => res.render('/profile', { theUser }))
-        .catch(err => ('Error en la BBDD', err))
+        .findByIdAndUpdate(req.params.id, {  username, password }, { new: true })
+        .then(() => res.redirect('/profile'))
+        .catch(err => console.log('BBDD error', err))
 })
 
 
 module.exports = router
+
